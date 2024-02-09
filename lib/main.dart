bff5fe281510e47af5cc6f29a43caf381b79a7a8
arefa313 <arefa.noori.1380@gmail.com>
2024-02-08 20:53:19 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,146 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(TodoApp());
 }
 
-class Task {
-  String title;
-  bool isCompleted;
-
-  Task(this.title, this.isCompleted);
-}
-
-class MyApp extends StatelessWidget {
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'To-Do List App',
+      title: 'Todo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TodoListScreen(),
+      home: TodoList(),
     );
   }
 }
 
-class TodoListScreen extends StatefulWidget {
+class TodoList extends StatefulWidget {
   @override
-  _TodoListScreenState createState() => _TodoListScreenState();
+  _TodoListState createState() => _TodoListState();
 }
 
-class _TodoListScreenState extends State<TodoListScreen> {
-  List<Task> _tasks = [];
+class _TodoListState extends State<TodoList> {
+  List<String> todos = [];
+
+  void _addTodo() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String newTodo = '';
+        return AlertDialog(
+          title: Text('Add Todo'),
+          content: TextField(
+            onChanged: (value) {
+              newTodo = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                setState(() {
+                  todos.add(newTodo);
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _removeTodo(int index) {
+    setState(() {
+      todos.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To-Do List'),
+        title: Text('Todo List'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: _tasks.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_tasks[index].title),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.check),
-                        onPressed: () {
-                          // Mark task as completed
-                          setState(() {
-                            _tasks[index].isCompleted = true;
-                          });
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () async {
-                          // Edit task
-                          String editedTask = await showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text('Edit Task'),
-                                content: TextFormField(
-                                  initialValue: _tasks[index].title,
-                                  onChanged: (value) {
-                                    // Store the edited task text
-                                     var editedTask = value;
-                                  },
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      // Close the dialog
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('Cancel'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      // Update task with edited text
-                                      setState(() {
-                                        _tasks[index].title = editedTask;
-                                      });
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('Save'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          // Delete task
-                          setState(() {
-                            _tasks.removeAt(index);
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  // Show a strike-through for completed tasks
-                  tileColor: _tasks[index].isCompleted
-                      ? Colors.grey[300]
-                      : Colors.transparent,
-                );
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(todos[index]),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                _removeTodo(index);
               },
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              onFieldSubmitted: (value) {
-                // Add a new task
-                setState(() {
-                  _tasks.add(Task(value, false));
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Add a task',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    // Add a new task (alternative way)
-                    setState(() {
-                      _tasks.add(Task(_tasks.length.toString(), false));
-                    });
-                  },
-                ),
-              ),
-            ),
-          ),
-        ],
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        tooltip: 'Add Todo',
+        child: Icon(Icons.add),
       ),
     );
   }
